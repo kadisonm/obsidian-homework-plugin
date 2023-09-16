8c61dab0d2635ecdd93b4dd6271ff8a00e794a62
Kadison <134670047+KadisonM@users.noreply.github.com>
2023-09-17 00:02:57 +0930

    Finished main features

--- modal.ts
+++ modal.ts
@@ -37,21 +37,90 @@ export default class HomeworkModal extends Modal {
                 btn
                 .setButtonText("✓")
                 .setCta()
-                .onClick(() => {
-                    
+                .onClick(() => {    
                     promptClass.empty();
                     this.createSubject(newSubjectName, true);
             }));
 		});
 	}
 
+	loadSubjects() {
+		let file = this.app.vault.getAbstractFileByPath(`${this.plugin.settings.homeworkPagePath}`);
+
+		if (file instanceof TFile) {
+			this.app.vault.read(file).then((fileContent) => {
+				let currentSubjectIndex = 0
+
+				while (true) {
+					// Find data
+
+					if (fileContent.length == 0)
+						break;
+
+					let firstSubjectStart = fileContent.indexOf("&", currentSubjectIndex);
+					let firstSubjectEnd = fileContent.indexOf("&", firstSubjectStart + 1);
+
+					if (firstSubjectEnd != -1) {
+						let subjectName = fileContent.substring(firstSubjectStart + 1, firstSubjectEnd);
+
+						let nextSubject = fileContent.indexOf("&", firstSubjectEnd + 1);
+
+						if (nextSubject == -1)
+							nextSubject = fileContent.length;
+
+						let subjectTasks = fileContent.substring(firstSubjectEnd + 1, nextSubject);
+
+						//Display Data
+
+						let subject = this.createSubject(subjectName, false);
+					
+						if (subjectTasks.length > 0)
+						{
+							let amountOfTasks = 0;
+
+							for (let i = 0; i < subjectTasks.length; i++) {
+								let letter = subjectTasks[i];
+
+								if (letter == "+")
+								{
+									amountOfTasks++;
+								}
+							}
+
+							amountOfTasks /= 2;
+
+							let taskIndex = 0;
+
+							for (let i = 0; i < amountOfTasks; i++) {
+								let taskStart = subjectTasks.indexOf("+", taskIndex);
+								let taskEnd = subjectTasks.indexOf("+", taskStart + 1);
+
+								let taskName = subjectTasks.substring(taskStart + 1, taskEnd);
+
+								this.createTask(taskName, subject, subjectName, false);
+
+								taskIndex = taskEnd + 1;
+
+								if (taskIndex == subjectTasks.length)
+									break;
+							}
+						}
+						
+						currentSubjectIndex = nextSubject;
+
+						if (currentSubjectIndex == fileContent.length)
+							break;	
+					}	
+					else {
+						break;
+					}
+				}
+			});
+		}
+	}
+
 	createSubject(name: string, newData: boolean) {
 		const {contentEl} = this;
-
-        let subject = contentEl.createEl("div", { cls: "subject" });
-
-		let subjectName = subject.createEl("div", {text: name, cls: "subject_name" });
-		let newTaskButton = subject.createEl("button", {text: "＋", cls: "subject_add", parent: subjectName });
 
 		if (newData)
 		{
@@ -59,31 +128,46 @@ export default class HomeworkModal extends Modal {
 
 			if (file instanceof TFile) {
 				this.writeSubjectFile(file, name);
-			}	
+			}		
 		}
+	
+		let subject = contentEl.createEl("div", { cls: "subject" });
+
+		let subjectName = subject.createEl("div", {text: name, cls: "subject_name" });
+		let newTaskButton = subject.createEl("button", {text: "＋", cls: "subject_add", parent: subjectName });
+		let removeSubjectButton = subject.createEl("button", {text: "X", cls: "subject_add", parent: subjectName });
+
+		removeSubjectButton.addEventListener("click", (click) => {
+			let file = this.app.vault.getAbstractFileByPath(`${this.plugin.settings.homeworkPagePath}`);
+
+			if (file instanceof TFile) {
+				subject.empty();
+				this.removeSubjectFile(file, name);
+			}	
+		});
 		
 		newTaskButton.addEventListener("click", (click) => {
-            let toDoName = "";
+			let toDoName = "";
 
-            let promptClass = subject.createEl("div", { cls: "promptClass" });
+			let promptClass = subject.createEl("div", { cls: "promptClass" });
 
-            let promptName = new Setting(promptClass)
-            .setName("New Task")
-            .addText((text) =>
-                text.onChange((value) => {
-                toDoName = value
-            }));
+			let promptName = new Setting(promptClass)
+			.setName("New Task")
+			.addText((text) =>
+				text.onChange((value) => {
+				toDoName = value
+			}));
 
-            let confirmName = new Setting(promptClass)
-            .addButton((btn) =>
-                btn
-                .setButtonText("✓")
-                .setCta()
-                .onClick(() => {  
-                    promptClass.empty();
+			let confirmName = new Setting(promptClass)
+			.addButton((btn) =>
+				btn
+				.setButtonText("✓")
+				.setCta()
+				.onClick(() => {  
+					promptClass.empty();
 
-                    this.createTask(toDoName, subject, name, true);
-            }));	
+					this.createTask(toDoName, subject, name, true);
+			}));	
 		});
 
 		return subject;
@@ -113,67 +197,6 @@ export default class HomeworkModal extends Modal {
 		});
     }
 
-	loadSubjects() {
-		let file = this.app.vault.getAbstractFileByPath(`${this.plugin.settings.homeworkPagePath}`);
-
-		if (file instanceof TFile) {
-			this.app.vault.read(file).then((fileContent) => {
-				let currentSubjectIndex = 0
-
-				while (true) {
-					// Find data
-
-					if (fileContent.length == 0)
-						break;
-
-					let firstSubjectStart = fileContent.indexOf("&", currentSubjectIndex);
-					let firstSubjectEnd = fileContent.indexOf("&", firstSubjectStart + 1);
-
-					let subjectName = fileContent.substring(firstSubjectStart + 1, firstSubjectEnd);
-
-					let nextSubject = fileContent.indexOf("&", firstSubjectEnd + 1);
-
-					if (nextSubject == -1)
-						nextSubject = fileContent.length;
-
-					let subjectTasks = fileContent.substring(firstSubjectEnd + 1, nextSubject);
-
-					//Display Data
-
-					let subject = this.createSubject(subjectName, false);
-
-					//this.createTask(toDoName, subject, subjectName, false);
-
-				
-					if (subjectTasks.length > 0)
-					{
-						let taskIndex = 0;
-
-						let count = (subjectTasks.match(new RegExp("+", "g")) || []).length;
-
-						new Notice(count.toString());
-
-						for (let i = 0; i < count / 2; i++) {
-							let taskStart = subjectTasks.indexOf("+", taskIndex);
-							let taskEnd = subjectTasks.indexOf("+", firstSubjectStart + 1);
-
-							let taskName = subjectTasks.substring(taskStart + 1, taskEnd);
-
-							taskIndex = taskEnd;
-
-							this.createTask(taskName, subject, subjectName, false);
-						}
-					}
-					
-					currentSubjectIndex = nextSubject;
-
-					if (currentSubjectIndex == fileContent.length)
-						break;	
-				}
-			});
-		}
-	}
-
 	writeSubjectFile(file: TFile, name: string ) {
 		this.app.vault.read(file).then((fileContent) => {
 			let subjectString = "&" + name + "&";
@@ -189,23 +212,43 @@ export default class HomeworkModal extends Modal {
 			let subjectName = "&" + subject + "&";
 			let position = fileContent.search(subjectName);
 
+			let subjectStart = fileContent.indexOf("&", position + 1);
+			let subjectEnd = fileContent.indexOf("&", subjectStart + 1);
+
+			if (subjectEnd == -1)
+				subjectEnd = fileContent.length;
+
 			if (position != -1) {
 				let toDoString = "+" + name + "+"
-				let newPosition = position + subjectName.length;
-				let newFileContent = fileContent.slice(0, newPosition) + toDoString + fileContent.slice(newPosition);
+				let newFileContent = fileContent.slice(0, subjectEnd) + toDoString + fileContent.slice(subjectEnd);
 				
 				this.app.vault.modify(file, newFileContent) 
 			}
 		});
 	}
 
-	removeSubjectFile(file: TFile, name: string ) {
+	removeSubjectFile(file: TFile, subject: string ) {
 		this.app.vault.read(file).then((fileContent) => {
-			let subjectString = "&" + name + "&";
+			let subjectName = "&" + subject + "&";
+			let subjectStart = fileContent.search(subjectName);
 
-			let newFileContent = fileContent + subjectString;
-			
-			this.app.vault.modify(file, newFileContent);
+			if (subjectStart != -1) {
+				let subjectNameEnd = fileContent.indexOf("&", subjectStart + 1);
+
+				if (subjectNameEnd != -1) {
+					let subjectEnd = fileContent.indexOf("&", subjectNameEnd + 1);;
+
+					if (subjectEnd == -1) {
+						subjectEnd = fileContent.length;
+					}
+
+					let subjectString = fileContent.substring(subjectStart, subjectEnd)
+
+					let newFileContent = fileContent.replace(subjectString, "")
+
+					this.app.vault.modify(file, newFileContent);
+				}
+			}
 		});
 	}
 
@@ -232,7 +275,6 @@ export default class HomeworkModal extends Modal {
 
 				if (taskPosition != -1)
 				{
-					new Notice("Found task");
 					let taskStart = startPosition + subjectName.length + taskPosition;
 					let taskEnd = taskStart + toDoString.length;
 
