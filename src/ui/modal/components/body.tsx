3916eaa4e76b67fcbad494d3a21192e8ddfc36a0
Kadison <134670047+KadisonM@users.noreply.github.com>
2024-04-02 12:48:32 +1030

    Add subject add task button

--- src/ui/modal/components/body.tsx
+++ src/ui/modal/components/body.tsx
@@ -7,15 +7,34 @@ import TaskComponent from "./task";
 export default function Body() {
     const props = useContext(HomeworkModalContext);
 
+    const taskLinkClicked = () => {
+        console.log('test')
+    }
+
     return (
         <div id="body">
             {props.views[props.currentView].subjects.map((subject: Subject, index: number)  => (
                 <div id="subject">
-                <h2>{subject.name}</h2>
-
-                {subject.tasks.map((task: Task, index: number)  => (
-                    <TaskComponent task={task}/>
-                ))}
+                    <div id="title"> 
+                        <h2>{subject.name}</h2>
+                        <IconButton icon={"plus"}/> 
+                    </div>
+                    
+                    {subject.tasks.map((task: Task, index: number)  => (
+                        <div id="task">
+                        {/* Left Div */}
+                        <div>
+                            <div id="check"/>
+                        </div>
+                        
+                        {/* Right Div */}
+                        <div>
+                            <p onClick={taskLinkClicked} className={task.page && "homework-link"} aria-label="Go to linked file" data-tooltip-position="right">
+                                {task.name}
+                            </p>
+                        </div>
+                    </div>
+                    ))}
                 </div>
             ))}
         </div>
