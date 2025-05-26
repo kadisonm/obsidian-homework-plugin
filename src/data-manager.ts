e345bfe655897edca2871935405d19d9c1fc345d
KadisonM <134670047+kadisonm@users.noreply.github.com>
2025-05-26 19:11:11 +0930

    Set up new data structure

--- src/data-manager.ts
+++ src/data-manager.ts
@@ -8,6 +8,7 @@ interface Item {
 
 export interface Project extends Item {
     readonly type: "Project";
+    children: string[];
 }
 
 export interface Section extends Item {
@@ -39,7 +40,7 @@ interface PluginData {
         showTooltips: boolean,
         version: string
     },
-    items: Array<Project | Section | Task>;  
+    items: Array<Project | Section | Task | SubTask>;  
     legacy?: {}
 }
 
@@ -66,18 +67,9 @@ export class DataManager {
     async load() {
 		const foundData = Object.assign({}, await this.plugin.loadData());
 
-        // Check for legacy data
-        if (!foundData.version) {
-            // Is either v1.0 or v1.1
-            if (foundData.settings.version)
-        }
+        // TODO: Migrate legacy data
 
-        // Migrate legacy data
-        if (foundData.homework === undefined) {
-            await this.migrateData(foundData);
-        } else {
-            this.data = Object.assign({}, DEFAULT_DATA, foundData);
-        }
+        this.data = Object.assign({}, DEFAULT_DATA, foundData);
 
 		await this.save();
 	}
@@ -86,97 +78,94 @@ export class DataManager {
 		await this.plugin.saveData(this.data);
 	}
 
-    // Convert legacy versions (1.0.0, 1.0.1) to new data structure
-    async migrateData(foundData: any) {
-        console.log("Legacy data found! Converting...");
-    
-        let newData = Object.assign({}, DEFAULT_DATA)
-        newData.legacy = foundData;
-    
-        this.data = newData;
-
-        const legacyData: any = Object.assign({}, this.data.legacy);
-
-        const view = await this.createItem<Subject>(
-            {
-                name: "View 1", 
-                parent: "root",
-            }
-        );
-
-        Object.keys(legacyData).forEach(async (subjectName: string, subjectIndex: number) => {
-            const subject = await this.createItem<Subject>(
-                {
-                    name: subjectName, 
-                    parent: view,
-                }
-            );
-    
-            const legacySubject = legacyData[subjectName];
-    
-            Object.keys(legacySubject).forEach(async (taskName: string, taskIndex: number) => {
-                const page = legacySubject[taskName].page;
-                const dueDate = new Date(legacySubject[taskName].date);
-
-                await this.createItem<Task>(
-                    {
-                        name: taskName, 
-                        page: page,
-                        date: dueDate,
-                        parent: subject,
-                    }
-                );
-            });
-        });
+    hasChildren(item: Item): Boolean{
+        return "children" in item;
     }
 
-    async createItem<Type extends View | Subject | Task>(args: Partial<Type> = {}) {
+    getItem(itemId: string) {
+        return this.data.items.find(({ id }) => id === itemId);
+    }
+
+    getItemIndex(itemId: string) {
+        const index = this.data.items.findIndex(({ id }) => id === itemId)
+
+        return index !== -1 ? index : undefined;
+    }
+
+    createItem<Type extends Project | Section | Task | SubTask>(args: Partial<Type> = {}) {
         const item = {...args} as Type;
 
         item.id = uuidv1();
-        item.parent = item.parent === undefined ? "root" : item.parent;
-        item.children = [];
+        
+        if (item.type === "Section" || item.type === "Task") {
+            item.parent = item.parent === undefined ? "root" : item.parent; 
+            item.children = [];
 
-        const parent = item.parent === "root" ? this.data.homework : this.data.homework.items.find(({ id }) => id === item.parent);
+            const parent = this.data.items.find(({ id }) => id === item.parent) as Project | Section | Task;
+            parent?.children.push(item.id);
+        }
 
-        this.data.homework.items.push(item);
-
-        parent?.children.push(item.id);
+        this.data.items.push(item);
 
         return item.id;
     }
 
-    async deleteItem(id: string) {
-        const searchId = id;
+    deleteItem(itemId: string) {
+        const itemIndex = this.getItemIndex(itemId);
 
-        const item = this.data.homework.items.findIndex(({ id }) => id === searchId);
+        if (!itemIndex) return;
 
-        for (const childId in this.data.homework.items[item].children) {
-            const child = this.data.homework.items.find(({ id }) => id === childId);
+        const item = this.data.items[itemIndex]
 
-            if (child !== undefined) {
-                child.parent = "root";
+        if (item.type === "Section" || item.type === "Task") {
+            for (const childId in item.children) {
+                const childIndex = this.getItemIndex(childId);
+
+                if (childIndex !== undefined) {
+                    this.data.items.splice(childIndex, 1);
+                }  
             }
         }
+        
+        this.data.items.splice(itemIndex, 1);
+    }
 
-        if (item !== undefined) {
-            this.data.homework.items.splice(item, 1);
+    changeItemParent(itemId: string, parentId: string) {
+        const item = this.getItem(itemId)
+
+        if (item && "parent" in item) {
+            const ogParent = this.getItem(item.parent);
+
+            if (ogParent && "children" in ogParent)
+                ogParent.children.remove(itemId);
+
+            item.parent = parentId;
+
+            const parent = this.getItem(parentId)
+
+            if (parent && "children" in parent)
+                parent.children.push(itemId);
         }
     }
 
-    async changeItemParent(id: string, parent: string) {
-        const searchId = id;
-        const itemIndex = this.data.homework.items.findIndex(({ id }) => id === searchId);
-        const item = this.data.homework.items[itemIndex];
+    reorderItem(itemId: string, targetIndex: number) {
+        const item = this.getItem(itemId)
 
-        // To-do: Make a function to replace this item finding process
-        const ogParentId = item.parent;
-        const ogParentIndex = this.data.homework.items.findIndex(({ id }) => id === ogParentId);
-        this.data.homework.items[ogParentIndex].children.remove(id);
+        if (item && "parent" in item) {
+            const parent = this.getItem(item.parent);
 
-        item.parent = parent;
+            if (parent && "children" in parent) {
+                const currentIndex = parent.children.findIndex(item => item === itemId);
+                
+                if (currentIndex === -1) return;
 
-        const parentIndex = this.data.homework.items.findIndex(({ id }) => id === parent);
-        this.data.homework.items[parentIndex].children.push(id);
+                const [deletedItem] = parent.children.splice(currentIndex, 1);
+
+                const clampedIndex = Math.clamp(targetIndex, 0, parent.children.length);
+
+                parent.children.splice(clampedIndex, 0, deletedItem);
+            }
+                
+        }
     }
 }
