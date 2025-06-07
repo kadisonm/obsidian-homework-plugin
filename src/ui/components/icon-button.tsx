5eb9df55ceab4588287ba883c94a33ee004509bb
Kadison <134670047+kadisonm@users.noreply.github.com>
2025-06-07 16:07:40 +0930

    Add eye toggle visual

--- src/ui/components/icon-button.tsx
+++ src/ui/components/icon-button.tsx
@@ -1,23 +1,37 @@
 import { setIcon } from 'obsidian';
-import { useRef, useLayoutEffect } from 'preact/hooks';
+import { useRef, useLayoutEffect, useState } from 'preact/hooks';
 
 interface Props {
     icon: string;
+    toggledIcon?: string;
     onClick?: any;
     attributeMessage?: string,
     attributePosition?: string
 };
 
-export function IconButton({ icon, onClick, attributeMessage, attributePosition }: Props) {
+export function IconButton({ icon, toggledIcon, onClick, attributeMessage, attributePosition }: Props) {
     const element = useRef(null);
+    const [toggled, setToggled] = useState(false)
 
     useLayoutEffect(() => {
+        if (toggledIcon === undefined) {
+            toggledIcon = icon;
+        }
+
         if (element.current) {
-            setIcon(element.current, icon);
+            setIcon(element.current, toggled ? toggledIcon : icon);
         } 
     });
 
+    const clicked = () => {
+        if (toggledIcon !== undefined) {
+            setToggled(!toggled);
+        }
+
+        onClick();
+    };
+
     return (   
-        <button ref={element} onClick={onClick} class="clickable-icon" aria-label={attributeMessage} data-tooltip-position={attributePosition}/>
+        <button ref={element} onClick={clicked} class="clickable-icon" aria-label={attributeMessage} data-tooltip-position={attributePosition}/>
     );
 }
--- src/ui/modal/header/index.tsx
+++ src/ui/modal/header/index.tsx
@@ -55,7 +55,7 @@ export default function Header() {
         <>
             <div class="header"> 
                 <ProjectSwitcher text={getProject()?.name ?? "Unnamed View"} attributeMessage="Switch projects" attributePosition="left" onClick={onDropdownClick}/> 
-                <IconButton icon='eye' attributeMessage="Show/hide completed tasks" attributePosition="top" onClick={onToggleCompletedTasks}/>
+                <IconButton icon='eye' toggledIcon="eye-off" attributeMessage="Show/hide completed tasks" attributePosition="top" onClick={onToggleCompletedTasks}/>
             </div>
             <div>
                 {showDropdownMenu && 
